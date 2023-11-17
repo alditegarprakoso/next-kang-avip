817804612472f8a9c3a596a7711cda2691e83a22
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2023-11-17 14:14:42 +0700

    change hit api with SWR

---
+++ pages-router/src/helper/helper.ts
@@ -0,0 +1,9 @@
+export const formatRupiah = (value: number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(value);
+};
+
+export const fetcher = (url: string) => fetch(url).then((res) => res.json());
--- pages-router/src/pages/product/index.tsx
+++ pages-router/src/pages/product/index.tsx
@@ -1,7 +1,11 @@
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
-import styles from "./Products.module.css";
 import Image from "next/image";
+import useSWR from "swr";
+
+import styles from "./Products.module.css";
+import { fetcher, formatRupiah } from "@/helper/helper";
+import SkeletonComponent from "@/components/elements/Skeleton";
 
 type ProductType = {
   id: number;
@@ -16,11 +20,11 @@ function ProductPage() {
   const [isLogin, setIsLogin] = useState(true);
   const [products, setProducts] = useState([]);
 
-  const fetchProducts = async () => {
-    const response = await fetch("/api/products");
-    const data = await response.json();
-    setProducts(data.data);
-  };
+  // const fetchProducts = async () => {
+  //   const response = await fetch("/api/products");
+  //   const data = await response.json();
+  //   setProducts(data.data);
+  // };
 
   useEffect(() => {
     if (!isLogin) {
@@ -28,24 +32,36 @@ function ProductPage() {
     }
   });
 
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+  // useEffect(() => {
+  //   fetchProducts();
+  // }, []);
+
+  const { data, error, isLoading } = useSWR("/api/products", fetcher);
 
   return (
     <>
       <div className={styles.container}>
         <h1>Product Page</h1>
-        {products.length > 0 && (
+        {!isLoading && data.data.length > 0 ? (
           <div className={styles.wrapCard}>
-            {products.map((product: ProductType) => (
+            {data.data.map((product: ProductType) => (
               <div key={product.id} className={styles.card}>
+                {/* <Image src={product.image} alt={product.name} width={200} height={200} /> */}
                 <img src={product.image} alt={product.name} />
-                <p className={styles.title}>{product.name}</p>
-                <p className={styles.price}>{product.price}</p>
                 <p className={styles.category}>{product.category}</p>
+                <p className={styles.name}>{product.name}</p>
+                <p className={styles.price}>{formatRupiah(product.price)}</p>
               </div>
             ))}
+          </div>
+        ) : (
+          <div className="flex mt-10 flex-wrap gap-8">
+            <SkeletonComponent />
+            <SkeletonComponent />
+            <SkeletonComponent />
+            <SkeletonComponent />
+            <SkeletonComponent />
+            <SkeletonComponent />
           </div>
         )}
       </div>
