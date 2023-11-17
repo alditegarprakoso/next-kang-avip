8be9470d378b1e7e4ee69272539115a92b0f5ca7
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2023-11-17 17:04:58 +0700

    change url fetch for static site generation

--- pages-router/src/pages/products/static.tsx
+++ pages-router/src/pages/products/static.tsx
@@ -1,16 +1,27 @@
-import { ProductType } from "../../types/product.type";
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  image: string;
+};
 
-export default function StaticRendering({ products }: { products: ProductType[]; }) {
+export default function StaticRendering({ products }: { products: Product[] }) {
   return (
     <div className="container px-8 py-6 ">
       <p className="mb-3">Static Site Generation</p>
-      <div className="flex gap-4 justify-center">
+      <div className="flex flex-wrap -mx-4">
         {products.map((product) => (
-          <div key={product.id}>
-            <img src={product.image} alt={product.name} />
-            <p>{product.category}</p>
-            <h2>{product.name}</h2>
-            <p>{product.price}</p>
+          <div
+            key={product.id}
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-4"
+          >
+            <div className="bg-white rounded-lg shadow-lg p-6">
+              <img src={product.image} alt={product.name} className="w-40" />
+              <p>{product.category}</p>
+              <h2>{product.title}</h2>
+              <p>{product.price}</p>
+            </div>
           </div>
         ))}
       </div>
@@ -20,12 +31,12 @@ export default function StaticRendering({ products }: { products: ProductType[];
 
 export async function getStaticProps() {
   // fetch data
-  const res = await fetch(`http://localhost:3000/api/products`);
+  const res = await fetch(`https://fakestoreapi.com/products`);
   const data = await res.json();
 
   return {
     props: {
-      products: data.data,
+      products: data,
     },
   };
 }
